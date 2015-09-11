2460662b4bbdc63b6cd839004e3d6e4cad11dfa0
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-09-11 23:36:27 +0530

    bbbb

--- Hackaton_D/src/hackaton_d/Hackaton_D.java
+++ Hackaton_D/src/hackaton_d/Hackaton_D.java
@@ -17,6 +17,9 @@ public class Hackaton_D {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi");
+        n obj = new n();
+        
+        obj.hesh();
     }
     
 }
